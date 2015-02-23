ec3dc7ec0b9e57cd61cfdd55f1162406db4acf98
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-23 17:37:05 +0000

    translate: docker stats translated into backstop format

--- translate/translate.go
+++ translate/translate.go
@@ -2,54 +2,44 @@
 package translate
 
 import (
-	"encoding/json"
+	"github.com/deliverous/collectd-docker/translate/backstop"
+	"github.com/deliverous/collectd-docker/translate/docker"
+	"time"
 )
 
-type ContainerStats struct {
-	Network *NetworkStats ``
-	Cpu     *CpuStats     `json:"cpu_stats"`
-	Memory  *MemoryStats  `json:"memory_stats"`
+func Translate(prefix string, stats *docker.ContainerStats) []backstop.Metric {
+	c := collector{Prefix: prefix, Time: time.Now(), Metrics: []backstop.Metric{}}
+	c.add("cpu.system", stats.Cpu.SystemCpuUsage)
+	c.add("cpu.total", stats.Cpu.CpuUsage.TotalUsage)
+	c.add("cpu.kernel", stats.Cpu.CpuUsage.UsageInKernelmode)
+	c.add("cpu.user", stats.Cpu.CpuUsage.UsageInUsermode)
+	c.add("memory.usage", stats.Memory.Usage)
+	c.add("memory.max_usage", stats.Memory.MaxUsage)
+	c.add("memory.limit", stats.Memory.Limit)
+	c.add("network.rx_bytes", stats.Network.RxBytes)
+	c.add("network.rx_packets", stats.Network.RxPackets)
+	c.add("network.rx_errors", stats.Network.RxErrors)
+	c.add("network.rx_dropped", stats.Network.RxDropped)
+	c.add("network.tx_bytes", stats.Network.TxBytes)
+	c.add("network.tx_packets", stats.Network.TxPackets)
+	c.add("network.tx_errors", stats.Network.TxErrors)
+	c.add("network.tx_dropped", stats.Network.TxDropped)
+
+	return c.Metrics
 }
 
-type NetworkStats struct {
-	RxBytes   int64 `json:"rx_bytes"`
-	RxPackets int64 `json:"rx_packets"`
-	RxErrors  int64 `json:"rx_errors"`
-	RxDropped int64 `json:"rx_dropped"`
-	TxBytes   int64 `json:"tx_bytes"`
-	TxPackets int64 `json:"tx_packets"`
-	TxErrors  int64 `json:"tx_errors"`
-	TxDropped int64 `json:"tx_dropped"`
+type collector struct {
+	Prefix  string
+	Time    time.Time
+	Metrics []backstop.Metric
 }
 
-type CpuStats struct {
-	CpuUsage       *CpuUsageStats `json:"cpu_usage"`
-	SystemCpuUsage int64          `json:"system_cpu_usage"`
+func (c *collector) add(name string, value *int64) {
+	if value != nil {
+		c.Metrics = append(c.Metrics, backstop.Metric{
+			Name:  c.Prefix + "." + name,
+			Value: *value,
+			Time:  c.Time,
+		})
+	}
 }
-
-type CpuUsageStats struct {
-	TotalUsage        int64 `json:"total_usage"`
-	UsageInKernelmode int64 `json:"usage_in_kernelmode"`
-	UsageInUsermode   int64 `json:"usage_in_usermode"`
-}
-
-type MemoryStats struct {
-	Usage    int64 `json:"usage"`
-	MaxUsage int64 `json:"max_usage"`
-	Limit    int64 `json:"limit"`
-}
-
-func ParseJson(data string) (*ContainerStats, error) {
-	var content *ContainerStats
-	err := json.Unmarshal([]byte(data), &content)
-	return content, err
-}
-
-/* Given : {
-"read":"2015-02-23T09:39:06.071088072+01:00",
-"network":{"rx_bytes":120062,"rx_packets":1425,"rx_errors":0,"rx_dropped":0,"tx_bytes":160652,"tx_packets":1257,"tx_errors":0,"tx_dropped":0},
-"cpu_stats":{"cpu_usage":{"total_usage":817842610809,"percpu_usage":[171131425370,170731715729,171006335590,171746607003,31328388915,34118247352,36020588573,31759302277],"usage_in_kernelmode":44460000000,"usage_in_usermode":100440000000},"system_cpu_usage":6616189560000000,"throttling_data":{"periods":0,"throttled_periods":0,"throttled_time":0}},
-"memory_stats":{"usage":361316352,"max_usage":600657920,"stats":{"active_anon":354676736,"active_file":6475776,"cache":6717440,"hierarchical_memory_limit":2147483648,"hierarchical_memsw_limit":4294967296,"inactive_anon":0,"inactive_file":163840,"mapped_file":32768,"pgfault":710237,"pgmajfault":0,"pgpgin":669598,"pgpgout":581386,"rss":354598912,"rss_huge":0,"swap":0,"total_active_anon":354676736,"total_active_file":6475776,"total_cache":6717440,"total_inactive_anon":0,"total_inactive_file":163840,"total_mapped_file":32768,"total_pgfault":710237,"total_pgmajfault":0,"total_pgpgin":669598,"total_pgpgout":581386,"total_rss":354598912,"total_rss_huge":0,"total_swap":0,"total_unevictable":0,"total_writeback":0,"unevictable":0,"writeback":0},"failcnt":0,"limit":2147483648},
-"blkio_stats":{"io_service_bytes_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_serviced_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_queue_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_service_time_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_wait_time_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_merged_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_time_recursive":[{"major":8,"minor":16,"op":"","value":0},{"major":8,"minor":0,"op":"","value":0}],"sectors_recursive":[{"major":8,"minor":16,"op":"","value":0},{"major":8,"minor":0,"op":"","value":0}]}
-}
-*/
--- translate/translate_test.go
+++ translate/translate_test.go
@@ -2,65 +2,77 @@
 package translate
 
 import (
+	"github.com/deliverous/collectd-docker/translate/docker"
 	"testing"
 )
 
-func Test_ParseJson_NetworkStats_Rx(t *testing.T) {
-	stats, _ := ParseJson(`{"network":{"rx_bytes":120062,"rx_packets":1425,"rx_errors":1,"rx_dropped":2}}`)
-	checkIntValue(t, "RxBytes", 120062, stats.Network.RxBytes)
-	checkIntValue(t, "RxPackets", 1425, stats.Network.RxPackets)
-	checkIntValue(t, "RxErrors", 1, stats.Network.RxErrors)
-	checkIntValue(t, "RxDropped", 2, stats.Network.RxDropped)
+func Test_CpuStats(t *testing.T) {
+	checkCpuStats(t, docker.CpuStats{SystemCpuUsage: v(100)}, "prefix.cpu.system", 100)
+	checkCpuStats(t, docker.CpuStats{CpuUsage: docker.CpuUsageStats{TotalUsage: v(3000)}}, "prefix.cpu.total", 3000)
+	checkCpuStats(t, docker.CpuStats{CpuUsage: docker.CpuUsageStats{UsageInKernelmode: v(2000)}}, "prefix.cpu.kernel", 2000)
+	checkCpuStats(t, docker.CpuStats{CpuUsage: docker.CpuUsageStats{UsageInUsermode: v(1000)}}, "prefix.cpu.user", 1000)
 }
 
-func Test_ParseJson_NetworkStats_Tx(t *testing.T) {
-	stats, _ := ParseJson(`{"network":{"tx_bytes":160652,"tx_packets":1257,"tx_errors":3,"tx_dropped":5}}`)
-	checkIntValue(t, "TxBytes", 160652, stats.Network.TxBytes)
-	checkIntValue(t, "TxPackets", 1257, stats.Network.TxPackets)
-	checkIntValue(t, "TxErrors", 3, stats.Network.TxErrors)
-	checkIntValue(t, "TxDropped", 5, stats.Network.TxDropped)
+func checkCpuStats(t *testing.T, stats docker.CpuStats, name string, value int64) {
+	checkTranslation(t, &docker.ContainerStats{Cpu: stats}, name, value)
 }
 
-func Test_ParseJson_CpuStats(t *testing.T) {
-	stats, _ := ParseJson(`{"cpu_stats":{"cpu_usage":{"total_usage":817842610809,"usage_in_kernelmode":44460000000,"usage_in_usermode":100440000000},"system_cpu_usage":6616189560000000}}`)
-
-	checkIntValue(t, "CupUsage.TotalUsage", 817842610809, stats.Cpu.CpuUsage.TotalUsage)
-	checkIntValue(t, "CupUsage.UsageInKernelmode", 44460000000, stats.Cpu.CpuUsage.UsageInKernelmode)
-	checkIntValue(t, "CupUsage.UsageInUsermode", 100440000000, stats.Cpu.CpuUsage.UsageInUsermode)
-	checkIntValue(t, "SystemCpuUsage", 6616189560000000, stats.Cpu.SystemCpuUsage)
+func Test_MemoryStats(t *testing.T) {
+	checkMemoryStats(t, docker.MemoryStats{Usage: v(100)}, "prefix.memory.usage", 100)
+	checkMemoryStats(t, docker.MemoryStats{MaxUsage: v(200)}, "prefix.memory.max_usage", 200)
+	checkMemoryStats(t, docker.MemoryStats{Limit: v(512)}, "prefix.memory.limit", 512)
 }
 
-func Test_ParseJson_MemoryStats(t *testing.T) {
-	stats, _ := ParseJson(`{"memory_stats":{ "usage":361316352, "max_usage":600657920, "limit":2147483648}}`)
-
-	checkIntValue(t, "Usage", 361316352, stats.Memory.Usage)
-	checkIntValue(t, "MaxUsage", 600657920, stats.Memory.MaxUsage)
-	checkIntValue(t, "Limit", 2147483648, stats.Memory.Limit)
+func checkMemoryStats(t *testing.T, stats docker.MemoryStats, name string, value int64) {
+	checkTranslation(t, &docker.ContainerStats{Memory: stats}, name, value)
 }
-func checkIntValue(t *testing.T, fieldName string, expected int64, got int64) {
-	if got != expected {
-		t.Errorf("'%s' verification failed: got %d, expected %d", fieldName, got, expected)
+
+func Test_NetworkStats(t *testing.T) {
+	checkNetworkStats(t, docker.NetworkStats{RxBytes: v(100)}, "prefix.network.rx_bytes", 100)
+	checkNetworkStats(t, docker.NetworkStats{RxPackets: v(100)}, "prefix.network.rx_packets", 100)
+	checkNetworkStats(t, docker.NetworkStats{RxErrors: v(100)}, "prefix.network.rx_errors", 100)
+	checkNetworkStats(t, docker.NetworkStats{RxDropped: v(100)}, "prefix.network.rx_dropped", 100)
+	checkNetworkStats(t, docker.NetworkStats{TxBytes: v(100)}, "prefix.network.tx_bytes", 100)
+	checkNetworkStats(t, docker.NetworkStats{TxPackets: v(100)}, "prefix.network.tx_packets", 100)
+	checkNetworkStats(t, docker.NetworkStats{TxErrors: v(100)}, "prefix.network.tx_errors", 100)
+	checkNetworkStats(t, docker.NetworkStats{TxDropped: v(100)}, "prefix.network.tx_dropped", 100)
+}
+
+func checkNetworkStats(t *testing.T, stats docker.NetworkStats, name string, value int64) {
+	checkTranslation(t, &docker.ContainerStats{Network: stats}, name, value)
+}
+
+func checkTranslation(t *testing.T, stats *docker.ContainerStats, name string, value int64) {
+	metrics := Translate("prefix", stats)
+	if len(metrics) != 1 {
+		t.Fatalf("translation failed, should have only one metric: got %#v", metrics)
+	}
+	if metrics[0].Name != name {
+		t.Errorf("translation failed: expected name '%s', got %s", name, metrics[0].Name)
+	}
+	if metrics[0].Value != value {
+		t.Errorf("translation failed: expected value '%d', got %d", value, metrics[0].Value)
 	}
 }
 
-const jsonContainerStats = `
-  {
-    "read":"2015-02-23T09:39:06.071088072+01:00",
-    "network":{"rx_bytes":120062,"rx_packets":1425,"rx_errors":1,"rx_dropped":2,"tx_bytes":160652,"tx_packets":1257,"tx_errors":3,"tx_dropped":5},
-    "cpu_stats":{
-      "cpu_usage":{
-        "total_usage":817842610809,
-        "percpu_usage":[171131425370,170731715729,171006335590,171746607003,31328388915,34118247352,36020588573,31759302277],
-        "usage_in_kernelmode":44460000000,
-        "usage_in_usermode":100440000000},
-      "system_cpu_usage":6616189560000000,
-      "throttling_data":{"periods":0,"throttled_periods":0,"throttled_time":0}},
-    "memory_stats":{
-      "usage":361316352,
-      "max_usage":600657920,
-      "stats":{"active_anon":354676736,"active_file":6475776,"cache":6717440,"hierarchical_memory_limit":2147483648,"hierarchical_memsw_limit":4294967296,"inactive_anon":0,"inactive_file":163840,"mapped_file":32768,"pgfault":710237,"pgmajfault":0,"pgpgin":669598,"pgpgout":581386,"rss":354598912,"rss_huge":0,"swap":0,"total_active_anon":354676736,"total_active_file":6475776,"total_cache":6717440,"total_inactive_anon":0,"total_inactive_file":163840,"total_mapped_file":32768,"total_pgfault":710237,"total_pgmajfault":0,"total_pgpgin":669598,"total_pgpgout":581386,"total_rss":354598912,"total_rss_huge":0,"total_swap":0,"total_unevictable":0,"total_writeback":0,"unevictable":0,"writeback":0},
-      "failcnt":0,
-      "limit":2147483648},
-    "blkio_stats":{"io_service_bytes_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_serviced_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_queue_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_service_time_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_wait_time_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_merged_recursive":[{"major":8,"minor":16,"op":"Read","value":0},{"major":8,"minor":16,"op":"Write","value":0},{"major":8,"minor":16,"op":"Sync","value":0},{"major":8,"minor":16,"op":"Async","value":0},{"major":8,"minor":16,"op":"Total","value":0},{"major":8,"minor":0,"op":"Read","value":0},{"major":8,"minor":0,"op":"Write","value":0},{"major":8,"minor":0,"op":"Sync","value":0},{"major":8,"minor":0,"op":"Async","value":0},{"major":8,"minor":0,"op":"Total","value":0}],"io_time_recursive":[{"major":8,"minor":16,"op":"","value":0},{"major":8,"minor":0,"op":"","value":0}],"sectors_recursive":[{"major":8,"minor":16,"op":"","value":0},{"major":8,"minor":0,"op":"","value":0}]}
-  }
-`
+func v(value int64) *int64 {
+	return &value
+}
+
+/*
+type ContainerStats struct {
+  → Network *NetworkStats ``
+}
+
+type NetworkStats struct {
+  → RxBytes   int64 `json:"rx_bytes"`
+  → RxPackets int64 `json:"rx_packets"`
+  → RxErrors  int64 `json:"rx_errors"`
+  → RxDropped int64 `json:"rx_dropped"`
+  → TxBytes   int64 `json:"tx_bytes"`
+  → TxPackets int64 `json:"tx_packets"`
+  → TxErrors  int64 `json:"tx_errors"`
+  → TxDropped int64 `json:"tx_dropped"`
+}
+
+*/
