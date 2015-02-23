e56c7b570d0ea8aff885e6a8dabddb8bc506a1bf
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-23 15:55:04 +0000

    backstop package added

---
+++ translate/backstop/backstop.go
@@ -0,0 +1,20 @@
+// vim: ts=2:nowrap
+package backstop
+
+import (
+	"encoding/json"
+	"time"
+)
+
+type Metric struct {
+	Name  string    `json:"metric"`
+	Value string    `json:"value"`
+	Time  time.Time `json:"measure_time"`
+}
+
+type Metrics []Metric
+
+func RenderMetrics(metrics []Metric) (string, error) {
+	data, err := json.Marshal(metrics)
+	return string(data), err
+}
---
+++ translate/backstop/backstop_test.go
@@ -0,0 +1,18 @@
+// vim: ts=2:nowrap
+package backstop
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_RenderMetrics(t *testing.T) {
+	now := time.Date(2015, time.January, 23, 15, 39, 06, 00, time.UTC)
+	cpu := Metric{Name: "cpu", Value: "42", Time: now}
+	memory := Metric{Name: "memory.enough", Value: "512", Time: now}
+	data, _ := RenderMetrics([]Metric{cpu, memory})
+
+	if data != `[{"metric":"cpu","value":"42","measure_time":"2015-01-23T15:39:06Z"},{"metric":"memory.enough","value":"512","measure_time":"2015-01-23T15:39:06Z"}]` {
+		t.Errorf("render metrics failed: got '%s'", data)
+	}
+}
