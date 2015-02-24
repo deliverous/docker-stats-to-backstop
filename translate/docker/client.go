e4d99ac26b558397fd108d04051ecb8be4ea8a00
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-24 12:15:46 +0000

    docker: client.go adde to get docker stats

---
+++ translate/docker/client.go
@@ -0,0 +1,14 @@
+// vim: ts=2 nowrap
+package docker
+
+import (
+	"gopkg.in/jmcvetta/napping.v1"
+)
+
+func GetDockerStats(session napping.Session, url string, stats *ContainerStats) (int, error) {
+	response, err := session.Get(url, nil, stats, nil)
+	if err != nil {
+		return 0, err
+	}
+	return response.Status(), nil
+}
