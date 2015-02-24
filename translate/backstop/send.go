8b3e717556265603cf911b62b4961a5f311907f0
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-24 12:14:43 +0000

    backstop: using gopkg.in/jmcvetta/napping.v1 as rest client api

--- translate/backstop/send.go
+++ translate/backstop/send.go
@@ -2,51 +2,13 @@
 package backstop
 
 import (
-	"net/http"
-	"strings"
+	"gopkg.in/jmcvetta/napping.v1"
 )
 
-func SendMetrics(url string, metrics []Metric, clientOptions []clientOption, requestOptions []requestOption) (status int, err error) {
-	data, err := RenderMetrics(metrics)
+func SendMetrics(session *napping.Session, url string, metrics []Metric) (int, error) {
+	response, err := session.Post(url, metrics, nil, nil)
 	if err != nil {
 		return 0, err
 	}
-
-	client := &http.Client{}
-	for _, option := range clientOptions {
-		option(client)
-	}
-
-	request, err := http.NewRequest("POST", url, strings.NewReader(data))
-	if err != nil {
-		return 0, err
-	}
-	for _, option := range requestOptions {
-		option(request)
-	}
-	request.Header.Set("Content-Type", "application/json")
-
-	response, err := client.Do(request)
-	if err != nil {
-		return 0, err
-	}
-	status = response.StatusCode
-	return
-}
-
-type clientOption func(client *http.Client)
-type requestOption func(request *http.Request)
-
-func client(options ...clientOption) []clientOption {
-	return options
-}
-
-func request(options ...requestOption) []requestOption {
-	return options
-}
-
-func BasicAuth(username, password string) requestOption {
-	return func(request *http.Request) {
-		request.SetBasicAuth(username, password)
-	}
+	return response.Status(), nil
 }
