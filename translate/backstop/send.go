81840825af8ec55d89dff40f40d9781b51ffd431
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-24 10:41:18 +0000

    backstop: SendMetrics has now options to customize client and request

--- translate/backstop/send.go
+++ translate/backstop/send.go
@@ -6,17 +6,47 @@ import (
 	"strings"
 )
 
-func SendMetrics(url string, metrics []Metric) (status int, err error) {
+func SendMetrics(url string, metrics []Metric, clientOptions []clientOption, requestOptions []requestOption) (status int, err error) {
 	data, err := RenderMetrics(metrics)
 	if err != nil {
 		return 0, err
 	}
 
-	client := http.Client{}
-	response, err := client.Post(url, "application/json", strings.NewReader(data))
+	client := &http.Client{}
+	for _, option := range clientOptions {
+		option(client)
+	}
+
+	request, err := http.NewRequest("POST", url, strings.NewReader(data))
+	if err != nil {
+		return 0, err
+	}
+	for _, option := range requestOptions {
+		option(request)
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := client.Do(request)
 	if err != nil {
 		return 0, err
 	}
 	status = response.StatusCode
 	return
+}
+
+type clientOption func(client *http.Client)
+type requestOption func(request *http.Request)
+
+func client(options ...clientOption) []clientOption {
+	return options
+}
+
+func request(options ...requestOption) []requestOption {
+	return options
+}
+
+func BasicAuth(username, password string) requestOption {
+	return func(request *http.Request) {
+		request.SetBasicAuth(username, password)
+	}
 }
