cadb425f341163a4f1889449a729106bebd472b1
Olivier Albiez <olivier.albiez@poloper.org>
2015-02-24 21:28:51 +0100

    hack removed from unix_socket

--- server/unix_socket.go
+++ server/unix_socket.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"errors"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -33,18 +31,7 @@ func (transport *socketTransport) RoundTrip(request *http.Request) (*http.Respon
 	}
 	socketClientConn := httputil.NewClientConn(dial, nil)
 	defer socketClientConn.Close()
-
-	response, err := socketClientConn.Do(request)
-
-	// Hack {
-	line, err := bufio.NewReader(response.Body).ReadString('\n')
-	if err != nil {
-		return nil, err
-	}
-	response.Body = ioutil.NopCloser(strings.NewReader(line))
-	// Hack }
-
-	return response, err
+	return socketClientConn.Do(request)
 }
 
 func split(urlPath string) (string, error) {
