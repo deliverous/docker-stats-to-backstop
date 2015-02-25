e260ef3fe212a21be0d28eed55d038f6eb3a2ea9
Olivier Albiez <olivier.albiez@poloper.org>
2015-02-25 17:21:18 +0100

    unit test added on prefix computation

--- server/server_test.go
+++ server/server_test.go
@@ -10,6 +10,7 @@ func Test_ComputePrefix(t *testing.T) {
 	ensurePrefix(t, "", container("id"), "id")
 	ensurePrefix(t, "", container("id", "the name"), "the name")
 	ensurePrefix(t, `{"regexp": "(.*)\\..*", "into": "prefix.$1"}`, container("id", "name.bla"), "prefix.name")
+	ensurePrefix(t, `{"regexp": "(.*)\\..*\\.(.*)", "into": "$1.$2"}`, container("id", "pid.bla.name"), "pid.name")
 }
 
 func ensurePrefix(t *testing.T, definition string, container docker.Container, expected string) {
