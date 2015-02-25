64fdd1c901ecf2793eb23c6b689c9e7fcee14dee
Olivier Albiez <olivier.albiez@deliverous.com>
2015-02-25 10:57:50 +0000

    unix_socket: test added

--- server/unix_socket_test.go
+++ server/unix_socket_test.go
@@ -8,6 +8,7 @@ import (
 
 func Test_UpdateUrl(t *testing.T) {
 	checkSuccess(t, url.URL{Path: "/var/run/docker.sock"}, url.URL{Host: "/var/run/docker.sock"})
+	checkSuccess(t, url.URL{Path: "/var/run/docker.sock/"}, url.URL{Host: "/var/run/docker.sock", Path: "/"})
 	checkSuccess(t, url.URL{Path: "/var/run/docker.sock/info"}, url.URL{Host: "/var/run/docker.sock", Path: "/info"})
 	checkSuccess(t, url.URL{Host: "var", Path: "/run/docker.sock/info"}, url.URL{Host: "/var/run/docker.sock", Path: "/info"})
 	checkError(t, url.URL{Path: "/no/socket"}, ErrNoSocket)
