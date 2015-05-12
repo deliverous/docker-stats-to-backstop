0214dd6cf7397decd1d6f30efe993fd33bee0bdc
Olivier Albiez <olivier.albiez@poloper.org>
2015-05-12 16:03:16 +0200

    unused code removed

--- translate/docker/container.go
+++ translate/docker/container.go
@@ -1,31 +1,7 @@
 // vim: ts=2 nowrap
 package docker
 
-import (
-	"strings"
-)
-
 type Container struct {
-	Id    string   ``
-	Image string   ``
-	Names []string ``
-}
-
-func (container Container) Name() string {
-	var name string
-	if len(container.Names) > 0 {
-		name = container.Names[0]
-	} else {
-		name = container.Id
-	}
-	count := strings.Count(name, "/")
-
-	for _, value := range container.Names {
-		current_count := strings.Count(value, "/")
-		if current_count < count {
-			count = current_count
-			name = value
-		}
-	}
-	return name
+	Id    string ``
+	Image string ``
 }
